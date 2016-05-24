9e96e9260d8a2502f97035d6ed8d19eff59ef167
Luweis <1372719400@qq.com>
2016-05-24 14:06:24 +0800

    提交运行时a
    第一次尝试

---
+++ 我的git/运行时/运行时/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  运行时
+//
+//  Created by wangmengmian on 16/3/30.
+//  Copyright © 2016年 wangmengmian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 我的git/运行时/运行时/ViewController.m
@@ -0,0 +1,54 @@
+//
+//  ViewController.m
+//  运行时
+//
+//  Created by wangmengmian on 16/3/30.
+//  Copyright © 2016年 wangmengmian. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <objc/runtime.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
++(void)initialize{
+    [self getIvars];
+}
++ (void)getIvars
+{
+    unsigned int count = 0;
+//    
+//    // 拷贝出所有的成员变量列表
+//    Ivar *ivars = class_copyIvarList([UITextField class], &count);
+//    
+//    for (int i = 0; i<count; i++) {
+//        // 取出成员变量
+//        //        Ivar ivar = *(ivars + i);
+//        Ivar ivar = ivars[i];
+//        
+//        // 打印成员变量名字
+//        //NSLog(@"%s %s", ivar_getName(ivar), ivar_getTypeEncoding(ivar));
+//    }
+//    
+    // 释放
+   // free(ivars);
+//    char count = class_getIvarLayout([UITextField class]);
+//    NSLog(@"%c",count);
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+   Method*motheds =  class_copyMethodList([UIButton class], &count);
+    for (int i=0; i<count; i++) {
+        Method mothe = motheds[i];
+        SEL sel = method_getName(mothe);
+        NSLog(@"%s",sel_getName(sel));
+    }
+   
+}
+
+@end
